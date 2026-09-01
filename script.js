e001bdbd7e1a14e73e871da6462db4e1a210664a
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-09-01 14:16:36 +0500

    chore: load final VeroSpace V5 behavior

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-(async()=>{await import('./base-script.js');await import('./cinematic.js');await import('./cinematic-tuning.js');})();
+(async()=>{await import('./base-script.js');await import('./cinematic.js');await import('./cinematic-tuning.js');await import('./final-v5.js');})();
